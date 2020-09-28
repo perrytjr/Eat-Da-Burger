d0a068117de0786e4ec0ca7c90ba3482a2f16d45
Perry Theobald <perrytjr@gmail.com>
2020-09-28 14:41:17 -0700

    created burgerjs file

--- models/burger.js
+++ models/burger.js
@@ -1,1 +1,31 @@
  var orm = require('../config/orm.js');
+
+
+ // this is the code that will call the ORM functions using specific burger input for the ORM. 
+
+ var burger = {
+    selectAll: function(cb) {
+      orm.selectAll("burgers", function(res) {
+        cb(res);
+      });
+    },
+    // The variables cols and vals are arrays.
+    insertOne: function(cols, vals, cb) {
+      orm.insertOne("burgers", cols, vals, function(res) {
+        cb(res);
+      });
+    },
+    updateOne: function(objColVals, condition, cb) {
+      orm.updateOne("burgers", objColVals, condition, function(res) {
+        cb(res);
+      });
+    },
+    deleteOne: function(condition, cb) {
+      orm.deleteOne("burgers", condition, function(res) {
+        cb(res);
+      });
+    }
+  };
+  
+  // Export the database functions for the controller (catsController.js).
+  module.exports = burger;
