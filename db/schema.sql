d4a53adf8e4031a5fca99350ee0955d3b092a1a4
Perry Theobald <perrytjr@gmail.com>
2020-09-28 12:40:54 -0700

    seedfilecreated

--- db/schema.sql
+++ db/schema.sql
@@ -11,4 +11,4 @@ CREATE TABLE burgers
     burger_name varchar(255) NOT NULL,
     devoured BOOLEAN DEFAULT false,
     PRIMARY KEY (id)
-)
+);
---
+++ db/seeds.sql
@@ -0,0 +1,4 @@
+INSERT INTO burgers (burger_name) VALUES ('Cheese Burger');
+INSERT INTO burgers (burger_name) VALUES ('Double Patty');
+INSERT INTO burgers (burger_name) VALUES ('Extra Cheese Burger');
+
